7d171bce88fc6a5d4143509021c8a17408735189
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-13 15:38:17 +0530

    Added set-up.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+m setuptools import setup, find_packages
+
+setup(
+    name='uploader',
+    version='0.1.1',
+    description='A Django file uploader',
+    author='woodman',
+    author_email='dana@danawoodman.com',
+    url='https://github.com/shaijudavis/Django-jQuery-File-Uploader-Integration-demo.git',
+    license='MIT',
+    packages=find_packages(),
+)
